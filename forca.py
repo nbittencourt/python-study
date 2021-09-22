f0607118903273a3292d9afc7c9189042798c252
Nicholas Bittencourt <nicholas@br-petrobras.com.br>
2021-09-22 14:55:54 -0300

    Avanço no jogo de forca

--- forca.py
+++ forca.py
@@ -3,6 +3,25 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print(20*"*")
 
+    palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+    
+    while (not enforcou and not acertou):
+        print(f"Essa é a palavra secreta: {letras_acertadas}")
+        chute = input("Escolha uma letra: ").lower().strip()
+
+        index = 0
+        result = ""
+        for letra in palavra_secreta:
+            if (chute == letra):
+                letras_acertadas[index] = letra
+
+            index = index + 1
+
+    print(f"Essa é a palavra secreta: {palavra_secreta} e você churou {letras_acertadas}")
     print("Fim de jogo...")
 
 if (__name__ == "__main__"):
